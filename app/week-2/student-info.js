0099ace25b65010617c70a910664773dab21abaf
Safa Nathani <safa.nathani@edu.sait.ca>
2025-09-17 22:25:10 -0600

    Complete week-2 assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Safa Nathani</p>
+      <p>
+        <Link href="https://github.com/safanathani/cprg306-assignments">
+          https://github.com/safanathani/cprg306-assignments
+        </Link>
+      </p>
+    </div>
+  );
+}
