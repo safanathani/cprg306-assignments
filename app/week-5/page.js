fa1f093f94fd21bf37dd157b51103353116871b7
Safa Nathani <safa.nathani@edu.sait.ca>
2025-10-10 15:47:22 -0600

    Week 5 Completed

---
+++ app/week-5/page.js
@@ -0,0 +1,9 @@
+import NewItem from "./new-item";
+
+export default function Page() {
+    return (
+        <main className = "min-h-screen flex items-start justify-center bg-black pt-6">
+            <NewItem />
+        </main>
+    );
+}
